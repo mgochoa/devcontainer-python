37bf4d0d4c71e958b3f6c74a65c8d7709ebe3ab1
Manuel Ochoa <mochoa1127@gmail.com>
2025-01-15 01:34:38 +0100

    add dev run

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello():
     return FileResponse("static/index.html")
+
+
+@app.post("/hello")
+def hello_post():
+    return "You posted something wow!"
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=9000, reload=True)
